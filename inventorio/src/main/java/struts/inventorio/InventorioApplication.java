d471a11d67ea0d84b35e3f4a9a18194855de4a84
hvaskjerikknoviler <Sverrejohansen@outlook.com>
2024-01-10 10:08:39 +0100

    First official commit

---
+++ inventorio/src/main/java/struts/inventorio/InventorioApplication.java
@@ -0,0 +1,13 @@
+package struts.inventorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorioApplication.class, args);
+	}
+
+}
---
+++ inventorio/src/test/java/struts/inventorio/InventorioApplicationTests.java
@@ -0,0 +1,13 @@
+package struts.inventorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
